371cd9009c15b699e8b221cf7d9f664131e52136
BlackPigeon <mihai.pricob@yahoo.com>
2023-03-24 00:46:33 +0200

    added 2 videos for demo

--- models/user.js
+++ models/user.js
@@ -21,16 +21,6 @@ const userSchema = new mongoose.Schema({
         required: false,
         default: 0
     }
-
-    /*  company: {
-        type: String,
-        enum: {
-            values: ["apple", "samsung", "dell", "mi"],
-            message: `{VALUE} is not supported`,
-        },
-    },
-*/
-
 })
 
 module.exports = mongoose.model('User', userSchema)
